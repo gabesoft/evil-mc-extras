ba3252ae129c3b79aeb70ec3d276cbda32b00421
Gabriel Adomnicai <gabesoft@gmail.com>
2017-02-02 08:49:03 -0800

    Increment cursor values weighted by the prefix argument. Fixes #2

--- evil-mc-extras-num.el
+++ evil-mc-extras-num.el
@@ -8,23 +8,21 @@
 (require 'evil-numbers)
 
 (defun evil-mc-change-num-at-each-cursor (change-cmd amount)
-  "Run CHANGE-CMD that changes the number at cursor by index and AMOUNT."
+  "Run CHANGE-CMD that changes the number at cursor by index times AMOUNT."
   (evil-mc-make-and-goto-first-cursor)
   (evil-mc-execute-for-all-cursors
    (lambda (cursor)
      (let* ((index (evil-mc-get-cursor-property cursor :index))
-            (value (+ index (or (and amount (prefix-numeric-value amount)) 0))))
+            (value (* index (or (and amount (prefix-numeric-value amount)) 1))))
        (funcall change-cmd value)))))
 
 (defun evil-mc-inc-num-at-each-cursor (amount)
-  "Increment the number at each active cursor by the cursor index
-with the first cursor starting at AMOUNT."
+  "Increment the number at each active cursor by the cursor index weighted by AMOUNT."
   (interactive "P")
   (evil-mc-change-num-at-each-cursor 'evil-numbers/inc-at-pt amount))
 
 (defun evil-mc-dec-num-at-each-cursor (amount)
-  "Decrement the number at each active cursor by the cursor index
-with the first cursor starting at AMOUNT."
+  "Decrement the number at each active cursor by the cursor index weighted by AMOUNT."
   (interactive "P")
   (evil-mc-change-num-at-each-cursor 'evil-numbers/dec-at-pt amount))
 
