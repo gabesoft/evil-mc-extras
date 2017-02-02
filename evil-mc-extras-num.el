23c67f8ccd2d7c254635b7ba19ad486c61946516
Gabriel Adomnicai <gabesoft@gmail.com>
2017-02-01 21:46:44 -0800

    Support prefix arguments for number commands. Fixes #2

--- evil-mc-extras-num.el
+++ evil-mc-extras-num.el
@@ -7,23 +7,26 @@
 (require 'evil-mc)
 (require 'evil-numbers)
 
-(defun evil-mc-change-num-at-each-cursor (change-cmd)
-  "Run CHANGE-CMD that changes the number at cursor."
+(defun evil-mc-change-num-at-each-cursor (change-cmd amount)
+  "Run CHANGE-CMD that changes the number at cursor by index and AMOUNT."
   (evil-mc-make-and-goto-first-cursor)
   (evil-mc-execute-for-all-cursors
    (lambda (cursor)
-     (let ((index (evil-mc-get-cursor-property cursor :index)))
-       (funcall change-cmd index)))))
+     (let* ((index (evil-mc-get-cursor-property cursor :index))
+            (value (+ index (or (and amount (prefix-numeric-value amount)) 0))))
+       (funcall change-cmd value)))))
 
-(defun evil-mc-inc-num-at-each-cursor ()
-  "Increment the number at each active cursor by the index amount."
-  (interactive)
-  (evil-mc-change-num-at-each-cursor 'evil-numbers/inc-at-pt))
+(defun evil-mc-inc-num-at-each-cursor (amount)
+  "Increment the number at each active cursor by the cursor index
+with the first cursor starting at AMOUNT."
+  (interactive "P")
+  (evil-mc-change-num-at-each-cursor 'evil-numbers/inc-at-pt amount))
 
-(defun evil-mc-dec-num-at-each-cursor ()
-  "Decrement the number at each active cursor by the index amount."
-  (interactive)
-  (evil-mc-change-num-at-each-cursor 'evil-numbers/dec-at-pt))
+(defun evil-mc-dec-num-at-each-cursor (amount)
+  "Decrement the number at each active cursor by the cursor index
+with the first cursor starting at AMOUNT."
+  (interactive "P")
+  (evil-mc-change-num-at-each-cursor 'evil-numbers/dec-at-pt amount))
 
 (provide 'evil-mc-extras-num)
 
